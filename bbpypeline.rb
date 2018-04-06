0d79006369445ccc6acf9786fcb5f61c4fd2ea39
Andrea Benfatti <WellDone2094@gmail.com>
2018-04-06 15:46:03 +0100

    add formula

---
+++ bbpypeline.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Bbpypeline < Formula
+  desc "Run bitbucket pipeline locally"
+  homepage ""
+  url "https://github.com/WellDone2094/bbpypeline/releases/download/v0.1.0/bbpypeline"
+  sha256 "ec9e4c803ba3752d80b99e244f41834be2fe7bcd02d3c16283df38b6424ba8d2"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "bbpypeline"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test bbpypeline`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
